8eceda59379a3b28105d52e14733320a95c4aabb
Fernando-Ibarra <fi94457@gmail.com>
2022-10-17 00:54:42 -0600

    [ADD - express]

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('My app');
+const express = require('express');
+// constructor - metodo que crea la aplicación
+const app = express();
+const port = 3000;
+
+
+app.get('/', (request, response) => {
+  response.send('Mi server en express');
+})
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}`);
+});
