0b6b38fdb981c5996cc31196258422b75a7454f3
Fernando-Ibarra <fi94457@gmail.com>
2022-10-17 01:00:46 -0600

    [ADD - routing]

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 app.get('/', (request, response) => {
   response.send('Mi server en express');
+})
+
+app.get('/nueva-ruta', (request, response) => {
+  response.send('Soy una nueva ruta')
+})
+
+app.get('/product', (request, response) => {
+  response.json({
+    name:'Product 1',
+    price: 1000
+  });
 })
 
 app.listen(port, () => {
