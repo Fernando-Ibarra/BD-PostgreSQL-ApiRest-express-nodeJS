f9b085227e019346ad4c586ab327e18643ce5bc6
Fernando-Ibarra <fi94457@gmail.com>
2022-10-17 12:34:25 -0600

    [ADD - GET2]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const productsRouter = require('./productsRouter');
 const userRouter = require('./usersRouter');
+const categoryRouter = require('./categoriesRouter');
 
 function routerApi(app) {
   app.use('/products', productsRouter);
   app.use('/users', userRouter);
+  app.use('/categories', categoryRouter);
 }
 
 module.exports = routerApi;
