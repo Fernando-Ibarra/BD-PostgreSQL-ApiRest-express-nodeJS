d5cc8a9db8a7c768e3868aa3212780b98ba94f29
Fernando-Ibarra <fi94457@gmail.com>
2022-10-26 16:14:14 -0600

    [ADD - IntroSequelize]

--- services/productService.js
+++ services/productService.js
@@ -1,13 +1,11 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgresPool')
+const sequelize = require('../libs/sequelize')
 class ProductsServices {
 
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (error) => console.error(error))
   }
 
   generate() {
@@ -34,8 +32,8 @@ class ProductsServices {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data, metadata] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
